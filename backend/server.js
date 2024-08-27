b9ecc5afe8ec6ea1de41af75351e07642dca3cd9
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-08-27 20:58:02 +0530

    backend jwt and protecting routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"; // Import CORS middleware
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
+
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -15,15 +18,19 @@ const app = express();
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Use CORS middleware
 app.use(cors());
+//cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Api is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 // Not Found and Error Handling Middlewares
 app.use(notFound);
